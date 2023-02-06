d7ab5da4921703cc2f5817dfbd377db1cd640cf5
lazaronazareno <lazaronazareno@gmail.com>
2023-02-06 13:14:38 -0300

    app congif, new styles, dependencies and utils

--- portfolio-client/src/App.jsx
+++ portfolio-client/src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar'
 import NavbarProfile from './components/navbar-profile'
-import ColorProvider from './context/colorContext'
-import { LanguageProvider } from './context/langContext'
+import PrivateRoute from './components/routes/privateRoute'
+import AdminPanel from './pages/admin'
 import Login from './pages/login'
 import Profile from './pages/profile'
 import Projects from './pages/projects'
 
+import useToken from './hooks/useToken'
+import { tokenAuth } from './config/tokenAuth'
+
+import ColorProvider from './context/colorContext'
+import { LanguageProvider } from './context/langContext'
+import AlertState from './context/alert/alertState'
+import AuthState from './context/auth/authState'
+import ProjectState from './context/projects/projectState'
+
 function App() {
+  const { token } = useToken()
+
+  if (token) {
+    tokenAuth(token)
+  }
   const [navToggled, setNavToggled] = useState(false)
 
   const handleNavToggle = () => {
@@ -19,34 +33,50 @@ function App() {
   return (
     <LanguageProvider>
       <ColorProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              exact path='/' element={
-                <>
-                  <NavbarProfile navToggled={navToggled} handleNavToggle={handleNavToggle} />
-                  <Profile />
-                </>
+        <AuthState>
+          <AlertState>
+            <ProjectState>
+              <BrowserRouter>
+                <Routes>
+                  <Route
+                    exact path='/' element={
+                      <>
+                        <NavbarProfile navToggled={navToggled} handleNavToggle={handleNavToggle} />
+                        <Profile />
+                      </>
             }
-            />
-            <Route
-              exact path='/projects' element={
-                <>
-                  <Navbar navToggled={navToggled} handleNavToggle={handleNavToggle} />
-                  <Projects />
-                </>
+                  />
+                  <Route
+                    exact path='/projects' element={
+                      <>
+                        <Navbar navToggled={navToggled} handleNavToggle={handleNavToggle} />
+                        <Projects />
+                      </>
             }
-            />
-            <Route
-              exact path='/login' element={
-                <>
-                  <Navbar navToggled={navToggled} handleNavToggle={handleNavToggle} />
-                  <Login />
-                </>
+                  />
+                  <Route
+                    exact path='/login' element={
+                      <>
+                        <Navbar navToggled={navToggled} handleNavToggle={handleNavToggle} />
+                        <Login />
+                      </>
             }
-            />
-          </Routes>
-        </BrowserRouter>
+                  />
+                  <Route exact element={<PrivateRoute />}>
+                    <Route
+                      exact path='/admin' element={
+                        <>
+                          <NavbarProfile navToggled={navToggled} handleNavToggle={handleNavToggle} />
+                          <AdminPanel />
+                        </>
+              }
+                    />
+                  </Route>
+                </Routes>
+              </BrowserRouter>
+            </ProjectState>
+          </AlertState>
+        </AuthState>
       </ColorProvider>
     </LanguageProvider>
   )
---
+++ portfolio-client/src/utils.js
@@ -0,0 +1,32 @@
+import * as Icons from 'react-icons/si'
+
+const stacks = {
+  html: 'SiHtml5',
+  css: 'SiCss3',
+  js: 'SiJss',
+  nodejs: 'SiNodedotjs',
+  react: 'SiReact',
+  mysql: 'SiMysql',
+  java: 'SiJava',
+  mongodb: 'SiMongodb',
+  nextjs: 'SiNextdotjs',
+  redux: 'SiRedux',
+  firebase: 'SiFirebase',
+  bootstrap: 'SiBootstrap',
+  docker: 'SiDocker',
+  angular: 'SiAngular',
+  express: 'SiExpress',
+  springboot: 'SiSpring',
+  typescript: 'SiTypescript'
+}
+
+export const DynamicIcon = ({ name, size }) => {
+  const iconName = stacks[name]
+  const IconComponent = Icons[iconName]
+
+  if (!IconComponent) {
+    return null
+  }
+
+  return IconComponent({ size })
+}
