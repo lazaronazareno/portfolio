f9cb3e113a2873ddb7171c42f194f9249d20aa09
lazaronazareno <lazaronazareno@gmail.com>
2023-11-27 12:13:55 -0300

    projects skeleton. description and cv link updated

---
+++ portfolio-client/src/components/loading/projectsLoading.jsx
@@ -0,0 +1,51 @@
+import './projectsLoading.css'
+
+const ProjectsLoading = () => {
+  return (
+    <article>
+      <div
+        className='article-image-section article-section'
+        style={{
+          backgroundImage:
+            'url(https://cdn-icons-png.flaticon.com/512/1160/1160358.png)',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundColor: 'grey',
+          // eslint-disable-next-line comma-dangle
+          minHeight: '12rem',
+        }}
+      />
+      <div
+        className='article-description-section article-section'
+        style={{ gap: '.5rem' }}
+      >
+        <div className='article-description-stack'>
+          <div className='icon-skeleton' />
+          <div className='icon-skeleton' />
+          <div className='icon-skeleton' />
+        </div>
+        <div className='p-skeleton' />
+        <div className='p-skeleton' />
+        <div className='p-skeleton' />
+      </div>
+      <div className='article-title-section article-section'>
+        <div className='title-skeleton' />
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+          <div className='button-skeleton' />
+          <div className='button-skeleton' />
+        </div>
+      </div>
+      <div className='article-nav-section article-section'>
+        <button className='arrow-skeleton'>
+          <div className='arrow-icon-skeleton left' />
+        </button>
+        <button className='arrow-skeleton '>
+          <div className='arrow-icon-skeleton right' />
+        </button>
+      </div>
+    </article>
+  )
+}
+
+export default ProjectsLoading
--- portfolio-client/src/pages/projects.jsx
+++ portfolio-client/src/pages/projects.jsx
@@ -1,10 +1,8 @@
+/* eslint-disable multiline-ternary */
 import React, { useContext, useEffect, useState } from 'react'
 import 'react-image-gallery/styles/css/image-gallery.css'
 /* import ReactImageGallery from 'react-image-gallery' */
-import {
-  FaArrowLeft,
-  FaArrowRight
-} from 'react-icons/fa'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 /* import { MdDoubleArrow } from 'react-icons/md' */
 import { DynamicIcon } from '../utils'
 
@@ -12,6 +10,7 @@ import { LanguageContext } from '../context/langContext'
 import ProjectContext from '../context/projects/projectContext'
 import AlertContext from '../context/alert/alertContext'
 import { Text } from '../lang/text'
+import ProjectsLoading from '../components/loading/projectsLoading'
 
 const Projects = () => {
   const projectsContext = useContext(ProjectContext)
@@ -24,7 +23,8 @@ const Projects = () => {
   const { userLanguage } = useContext(LanguageContext)
 
   const handleLeftClick = () => {
-    const previousIndex = activeIndex === 0 ? projects.length - 1 : activeIndex - 1
+    const previousIndex =
+      activeIndex === 0 ? projects.length - 1 : activeIndex - 1
 
     setActiveIndex(previousIndex)
   }
@@ -44,30 +44,29 @@ const Projects = () => {
 
   return (
     <main>
-      {alert
-        ? (
-          <div className={`error-alert ${alert.category}`}>
-            {alert.msg}
-          </div>
-          )
-        : null}
-      {projects.map((item, index) => (
-        <article
-          key={index}
-          data-index={index}
-          data-status={activeIndex === index ? 'active' : 'inactive'}
-        >
-          <div
-            className='article-image-section article-section'
-            style={{
-              backgroundImage: `url(${item.images[0].original})`,
-              backgroundSize: 'contain',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              minHeight: '12rem'
-            }}
-          >
-            {/*             <ReactImageGallery
+      {alert ? (
+        <div className={`error-alert ${alert.category}`}>{alert.msg}</div>
+      ) : null}
+      {projects.length > 0 ? (
+        <>
+          {projects.map((item, index) => (
+            <article
+              key={index}
+              data-index={index}
+              data-status={activeIndex === index ? 'active' : 'inactive'}
+            >
+              <div
+                className='article-image-section article-section'
+                style={{
+                  backgroundImage: `url(${item.images[0].original})`,
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                  // eslint-disable-next-line comma-dangle
+                  minHeight: '12rem',
+                }}
+              >
+                {/*             <ReactImageGallery
               items={item.images}
               alt={userLanguage === 'es' ? item.titleEs : item.titleEn}
               showThumbnails={false}
@@ -80,46 +79,82 @@ const Projects = () => {
                 <MdDoubleArrow size={92} className='image-right-nav' onClick={onClick} disabled={disabled} />
               )}
             /> */}
-          </div>
-          <div className='article-description-section article-section' style={{ gap: '.5rem' }}>
-            <div className='article-description-stack'>
-              {item.stack.map(stack => {
-                return <DynamicIcon title={stack} key={stack} name={stack} size={64} />
-              })}
-            </div>
-            <p>{userLanguage === 'es' ? item.descriptionEs : item.descriptionEn}</p>
-          </div>
-          <div className='article-title-section article-section'>
-            <h2>{userLanguage === 'es' ? item.titleEs : item.titleEn}</h2>
-            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-              {item.deploy && (
-                <a type='button' href={item.deploy} target='_blank' rel='noopener noreferrer' className='button'><Text tid='deployButton' /></a>
-              )}
-              <a type='button' href={item.repo} target='_blank' rel='noopener noreferrer' className='alt-button'><Text tid='repoButton' /></a>
-            </div>
-          </div>
-          <div className='article-nav-section article-section'>
-            <button
-              className='article-nav-button'
-              type='button'
-              onClick={handleLeftClick}
-              title='arrow'
-              disabled={projects.length <= 1}
-            >
-              <FaArrowLeft size={92} />
-            </button>
-            <button
-              className='article-nav-button'
-              type='button'
-              onClick={handleRightClick}
-              title='arrow'
-              disabled={projects.length <= 1}
-            >
-              <FaArrowRight size={92} />
-            </button>
-          </div>
-        </article>
-      ))}
+              </div>
+              <div
+                className='article-description-section article-section'
+                style={{ gap: '.5rem' }}
+              >
+                <div className='article-description-stack'>
+                  {item.stack.map((stack) => {
+                    return (
+                      <DynamicIcon
+                        title={stack}
+                        key={stack}
+                        name={stack}
+                        size={64}
+                      />
+                    )
+                  })}
+                </div>
+                <p>
+                  {userLanguage === 'es'
+                    ? item.descriptionEs
+                    : item.descriptionEn}
+                </p>
+              </div>
+              <div className='article-title-section article-section'>
+                <h2>{userLanguage === 'es' ? item.titleEs : item.titleEn}</h2>
+                <div
+                  style={{ display: 'flex', justifyContent: 'space-evenly' }}
+                >
+                  {item.deploy && (
+                    <a
+                      type='button'
+                      href={item.deploy}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='button'
+                    >
+                      <Text tid='deployButton' />
+                    </a>
+                  )}
+                  <a
+                    type='button'
+                    href={item.repo}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='alt-button'
+                  >
+                    <Text tid='repoButton' />
+                  </a>
+                </div>
+              </div>
+              <div className='article-nav-section article-section'>
+                <button
+                  className='article-nav-button'
+                  type='button'
+                  onClick={handleLeftClick}
+                  title='arrow'
+                  disabled={projects.length <= 1}
+                >
+                  <FaArrowLeft size={92} />
+                </button>
+                <button
+                  className='article-nav-button'
+                  type='button'
+                  onClick={handleRightClick}
+                  title='arrow'
+                  disabled={projects.length <= 1}
+                >
+                  <FaArrowRight size={92} />
+                </button>
+              </div>
+            </article>
+          ))}
+        </>
+      ) : (
+        <ProjectsLoading />
+      )}
     </main>
   )
 }
